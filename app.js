2eb1ff6519faeaeca4f5d87616891de74eb7a785
aylindoganer <doganeraylinn@gmail.com>
2022-08-19 13:44:00 +0300

    final

--- app.js
+++ app.js
@@ -5,39 +5,29 @@ const adviceContainer = document.querySelector('.container');
 const adviceText = document.querySelector('h2')
 const adviceURL = 'https://api.adviceslip.com/advice';
 
+
+
+adviceBtn.addEventListener('click', getAdvice)
+
+
 // fetch API
 
 async function getAdvice () {
 
     const response = await fetch(adviceURL);
-    const data = await response.json(); 
+    const data = await response.json();
+    const {slip} = data;
     const slipAdvice = slip.advice;
+    
+    adviceText.textContent = slipAdvice;
     console.log(data);
     console.log(slip);
     console.log(slipAdvice);
 
-
-    adviceBtn.addEventListener('click',function(){
     
-    // random advice --copied this from stack overflow
-    // picks random letters lol 
-    // idk what's going on here really
-
-    const adviceArray = Object.keys(slip.advice);
-    const randomNumber = Math.random();
-    const adviceIndex = Math.floor(randomNumber * adviceArray.length);
-    const randomKey = adviceArray[adviceIndex];
-    const randomValue = slipAdvice[randomKey];
-    console.log(randomValue);
-
-    // change the text content 
-
-         adviceText.textContent = randomValue;
-    })
 
 }
 
-getAdvice();
 
 
 
@@ -45,8 +35,3 @@ getAdvice();
 
 
 
-
-
-
-
- 
